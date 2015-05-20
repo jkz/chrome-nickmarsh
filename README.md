4db944fb096962452c9d26effeb914a225e6b17f
Jesse Zwaan <j.k.zwaan@gmail.com>
2015-05-20 16:07:26 +0100

    Nick Marsh

---
+++ README.md
@@ -0,0 +1,4 @@
+chrome-nick-marsh
+===========
+
+Nick Marsh
---
+++ content/nickmarsh.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  var count = $("body *").replaceText(/design/gi, '<span>Nick Marsh</span>', false,  true);
+  console.log("NICK MARSH");
+});
+
+
